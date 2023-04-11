0ed7a315fb7ba81803b9c532c8941586ab698d30
michael <fuck@qq.com>
2023-04-11 21:14:59 +0800

    the second test

--- app/src/main/java/cn/edu/jnu/x2020101343/BowlingGame.java
+++ app/src/main/java/cn/edu/jnu/x2020101343/BowlingGame.java
@@ -1,11 +1,12 @@
 package cn.edu.jnu.x2020101343;
 
 public class BowlingGame {
-
+    private int score = 0;
     public void roll(int pin) {
+        score += pin;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- app/src/test/java/cn/edu/jnu/x2020101343/BowlingGameUnitTest.java
+++ app/src/test/java/cn/edu/jnu/x2020101343/BowlingGameUnitTest.java
@@ -1,5 +1,6 @@
 package cn.edu.jnu.x2020101343;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,12 +11,26 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void test20Zeros() {
-        BowlingGame game = new BowlingGame();
         for(int i=0;i<20;i++){
             game.roll(0);
         }
         assertEquals(0,game.score());
+    }
+    @Test
+    public void testAllOnes() {
+        for(int i=0;i<20;i++){
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
     }
 }
